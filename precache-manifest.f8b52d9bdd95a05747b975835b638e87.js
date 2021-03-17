c4a5764a1835d666d8a500e0dae90c8f3dbf7eb4
yaztaha <yazi.taha@gmail.com>
2021-03-17 11:25:24 +0100

    Updates

---
+++ precache-manifest.f8b52d9bdd95a05747b975835b638e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af59bac6b7e3a53bbba595b7496e0b54",
+    "url": "/Portfolio-NasaApi/index.html"
+  },
+  {
+    "revision": "9fe77f48e6fa123de889",
+    "url": "/Portfolio-NasaApi/static/css/main.d6273c52.chunk.css"
+  },
+  {
+    "revision": "a1359aa702a736fe8573",
+    "url": "/Portfolio-NasaApi/static/js/2.e1676f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-NasaApi/static/js/2.e1676f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe77f48e6fa123de889",
+    "url": "/Portfolio-NasaApi/static/js/main.8b870d5a.chunk.js"
+  },
+  {
+    "revision": "d914f5d02fc0676fcbb3",
+    "url": "/Portfolio-NasaApi/static/js/runtime-main.a5368592.js"
+  }
+]);
